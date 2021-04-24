59c3e19206df0c820fbcd30996dc5fbf0fba1187
barabum666 <65859711+barabum666@users.noreply.github.com>
2021-04-24 21:21:09 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,226 @@
+/*------BURGER------*/
+
+let burger = document.querySelector(".burger");
+let nav = document.querySelector(".main-nav");
+
+burger.addEventListener("click", function () {
+	burger.classList.toggle("active");
+	nav.classList.toggle("active");
+});
+
+if (window.innerWidth <= 992) {
+	let parent = document.querySelectorAll(".product-item");
+	let child = document.querySelectorAll(".add-to-cart-btn");
+
+	for (i = 0; i < parent.length; i++) {
+		parent[i].append(child[i]);
+	}
+}
+
+if (window.innerWidth <= 767) {
+	document.querySelector(".main-nav").prepend(document.querySelector(".header-top__content-left"));
+	document.querySelector(".main-nav-icons").prepend(document.querySelector(".header-content__enter-buttons"));
+	document.querySelector(".header-content__enter-buttons").append(document.querySelector(".tel-icon"));
+	document.querySelector(".main-nav").prepend(document.querySelector(".main-nav-icons"));
+	document.querySelector(".main-nav").append(document.querySelector(".header-top__content-right"));
+}
+
+let phone = document.querySelector(".tel-icon");
+let options = document.querySelectorAll(".main-nav .call-back-link");
+
+phone.addEventListener("click", function () {
+	for (i = 0; i < options.length; i++) {
+		options[i].classList.toggle("active");
+	}
+});
+
+/*-------SLIDER-------*/
+
+new Swiper(".baner-init", {
+	// Arrows
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev"
+	},
+	// Bullets
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true
+	},
+	watchOverflow: true,
+	// Infinite
+	loop: true,
+	// Autoplay
+	autoplay: {
+		delay: 4000,
+		disableOnInteraction: false
+	},
+	// Change speed
+	speed: 300,
+});
+
+/*---------popup-product-description-link---*/
+
+const productDescriptionLinks = document.querySelectorAll(".popup-product-description-link");
+
+if (productDescriptionLinks.length > 0) {
+	for (i = 0; i < productDescriptionLinks.length; i++) {
+		const productDescriptionLink = productDescriptionLinks[i];
+		productDescriptionLink.addEventListener('click', function () {
+			const currentActive = document.querySelector(".popup-product-description-text.active");
+			if (currentActive) {
+				currentActive.classList.remove("active");
+			}
+			const productDescriptionName = productDescriptionLink.getAttribute('href').replace('#', '');
+			const currentDescription = document.getElementById(productDescriptionName);
+			currentDescription.classList.add("active");
+
+			const currentActiveLink = document.querySelector(".popup-product-description-link.active");
+			if (currentActiveLink) {
+				currentActiveLink.classList.remove("active");
+			}
+			productDescriptionLink.classList.add("active");
+		});
+	}
+}
+
+/*---------Disabled subscription button---*/
+
+const subBtn = document.querySelectorAll(".subscription-button");
+const subCheck = document.querySelectorAll(".agreement-check");
+
+function isChecked() {
+	for (i = 0; i < subBtn.length; i++) {
+		if (subCheck[i].checked && subBtn[i].disabled) {
+			subBtn[i].toggleAttribute("disabled");
+		}
+		else if (subCheck[i].checked == false && subBtn[i].disabled == false) {
+			subBtn[i].toggleAttribute("disabled");
+		}
+	}
+};
+
+if (subCheck.length > 0) {
+	for (let i = 0; i < subCheck.length; i++) {
+		const currentCheck = subCheck[i];
+		currentCheck.addEventListener("click", isChecked);
+	}
+}
+
+window.onload = isChecked;
+
+/*-----------Popup-----------------*/
+
+const popupLinks = document.querySelectorAll('.popup-link');
+const body = document.querySelector('body');
+const lockPadding = document.querySelectorAll('.lock-padding');
+
+let unlock = true;
+
+const timeout = 800;
+
+if (popupLinks.length > 0) {
+	for (let i = 0; i < popupLinks.length; i++) {
+		const popupLink = popupLinks[i];
+		popupLink.addEventListener('click', function (e) {
+			const popupName = popupLink.getAttribute('href').replace('#', '');
+			const currentPopup = document.getElementById(popupName);
+			popupOpen(currentPopup);
+			e.preventDefault();
+		});
+	}
+}
+
+const popupCloseIcon = document.querySelectorAll('.close-popup');
+
+if (popupCloseIcon.length > 0) {
+	for (let i = 0; i < popupCloseIcon.length; i++) {
+		const el = popupCloseIcon[i];
+		el.addEventListener('click', function (e) {
+			popupClose(el.closest('.popup'));
+			e.preventDefault();
+		});
+	}
+}
+
+function popupOpen(currentPopup) {
+	if (currentPopup && unlock) {
+		const popupActive = document.querySelector('.popup.open');
+		if (popupActive) {
+			popupClose(popupActive, false);
+		} else {
+			bodyLock();
+		}
+		currentPopup.classList.add('open');
+		currentPopup.addEventListener('click', function (e) {
+			if (!e.target.closest('.popup-content')) {
+				popupClose(e.target.closest('.popup'));
+			}
+		});
+
+		/*-------SLIDER product-popup-------*/
+		new Swiper(".popup-product-slider", {
+			pagination: {
+				el: ".popup-product-swiper-pagination",
+				clickable: true,
+			},
+			watchOverflow: true,
+			loop: true,
+			centeredSlides: true,
+			observeParents: true,
+		});
+
+	}
+}
+
+function popupClose(popupActive, doUnlock = true) {
+	if (unlock) {
+		popupActive.classList.remove('open');
+		if (doUnlock) {
+			bodyUnlock();
+		}
+	}
+}
+
+function bodyLock() {
+	const lockPaddingValue = window.innerWidth - document.querySelector('body').offsetWidth + 'px';
+
+	if (lockPadding.length > 0) {
+		for (let i = 0; i < lockPadding.lenth; i++) {
+			const el = lockPadding[i];
+			el.style.paddingRight = lockPaddingValue;
+		}
+	}
+	body.style.paddingRight = lockPaddingValue;
+	body.classList.add('lock');
+
+	unlock = false;
+	setTimeout(function () {
+		unlock = true;
+	}, timeout);
+}
+
+function bodyUnlock() {
+	setTimeout(function () {
+		if (lockPadding.length > 0) {
+			for (let i = 0; i < lockPadding.length; i++) {
+				const el = lockPadding[i];
+				el.style.paddingRight = '0px';
+			}
+		}
+		body.style.paddingRight = '0px';
+		body.classList.remove('lock');
+	}, timeout);
+
+	unlock = false;
+	setTimeout(function () {
+		unlock = true;
+	}, timeout);
+}
+
+document.addEventListener('keydown', function (e) {
+	if (e.which === 27) {
+		const popupActive = document.querySelector('.popup.open');
+		popupClose(popupActive);
+	}
+})
